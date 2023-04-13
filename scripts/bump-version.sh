37ddf1e9e118a825983e35040da228406e0a1e18
Nold <Nold360@users.noreply.github.com>
2023-04-13 11:46:28 +0200

    Update bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -u
+set -e
+
 chart="$1"
-VERSION=$(awk '/^version:/ { print $2 }' Chart.yaml)
+VERSION=$(awk '/^version:/ { print $2 }' "${chart}/Chart.yaml")
 NEWVERSION=$(echo $VERSION | awk -F. '/[0-9]+\./{$NF++;print}' OFS=.)
 sed "s/$VERSION/$NEWVERSION/" -i "${chart}/Chart.yaml"
 git add "${chart}/Chart.yaml"
